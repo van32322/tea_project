8800cfad8f21a901bf712ad8cfec0cfcfbba0ab3
Tran Vi <phanminhvan13@gmail.com>
2026-05-15 22:11:34 +0700

    mfinish

--- FE/src/services/productService.js
+++ FE/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/products';
+const API_URL = import.meta.env.VITE_API_URL + '/api/products' || 'http://127.0.0.1:8000/api/products';
 
 export const productService = {
     getAllProducts: async () => {
